ccbd2231f6437eda0c923681fc2e3bd9c4cc6b71
sasaki <skyo.97.admj@gmail.com>
2021-10-23 14:49:17 +0900

    Label

---
+++ frontend/components/Atoms/Label.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="label-container">
+    <span class="label-title">{{ labelName }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    labelName: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.label {
+  &-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50px;
+    height: 20px;
+    border-radius: 15px;
+    background: #2f9447;
+    color: #fff;
+    font-size: 12px;
+    font-weight: bold;
+  }
+}
+</style>
