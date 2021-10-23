de12a74b3c8ef0bccee7e94407e17a7309fa5802
sasaki <skyo.97.admj@gmail.com>
2021-10-23 09:46:28 +0900

    LIFFアプリを初期化する

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,36 +1,17 @@
-<template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        frontend
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
-</template>
+<template></template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { defineComponent, onMounted } from '@nuxtjs/composition-api';
 
-export default Vue.extend({})
+export default defineComponent({
+  setup() {
+    onMounted(async () => {
+      await liff.init({
+        liffId: process.env.LIFF_ID,
+      });
+    });
+  },
+});
 </script>
 
 <style>
@@ -44,16 +25,8 @@ export default Vue.extend({})
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
