ba02d53b04ebc4e61cb3ce4b664b9ed9dcaf820b
sasaki <skyo.97.admj@gmail.com>
2021-10-23 21:03:34 +0900

    FormQuestion

---
+++ frontend/components/Atoms/FormQuestion.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="formQuestion-container">
+    <h3 class="formQuestion-text">{{ question }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    question: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.formQuestion {
+  &-text {
+    color: $color_black;
+    font-size: 12px;
+    font-weight: bold;
+  }
+}
+</style>
