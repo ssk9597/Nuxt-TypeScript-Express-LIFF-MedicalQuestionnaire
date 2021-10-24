eb47b1fafdd189691333829f47c4f4eac0107175
sasaki <skyo.97.admj@gmail.com>
2021-10-24 09:36:26 +0900

    FormButton

---
+++ frontend/components/Atoms/FormButton.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="formButton-container">
+    <button
+      class="formButton-button"
+      :style="{
+        backgroundColor: backgroundColor,
+        fontSize: fontSize,
+        maxWidth: maxWidth,
+      }"
+      @click="childClick"
+    >
+      {{ buttonText }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    backgroundColor: {
+      type: String,
+      default: '#CE6752',
+    },
+    fontSize: {
+      type: String,
+      default: '24px',
+    },
+    maxWidth: {
+      type: String,
+      default: '250px',
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    },
+    click: {
+      type: Function,
+      required: true,
+    },
+  },
+  methods: {
+    childClick() {
+      this.click();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.formButton {
+  &-container {
+    display: flex;
+    justify-content: center;
+  }
+
+  &-button {
+    cursor: pointer;
+    width: 100%;
+    padding: 8px 0;
+    font-weight: bold;
+    color: $color_white;
+    text-decoration: none;
+    user-select: none;
+    border: 1px $color_white solid;
+    border-radius: 15px;
+    transition: 0.4s ease;
+
+    &:hover {
+      opacity: 0.9;
+    }
+  }
+}
+</style>
