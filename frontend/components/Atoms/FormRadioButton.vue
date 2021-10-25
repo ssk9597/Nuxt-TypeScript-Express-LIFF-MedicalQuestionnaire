a29d4c701466fa823007f85103a5e5be093c7ee9
sasaki <skyo.97.admj@gmail.com>
2021-10-25 09:34:45 +0900

    FormRadioButton

---
+++ frontend/components/Atoms/FormRadioButton.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="radio-container">
+    <div class="radio-wrapper" v-for="(option, index) in options" :key="index">
+      <input
+        class="radio-wrapper-input"
+        type="radio"
+        v-model="chooseVal"
+        :value="option"
+        :id="option"
+        @change="$emit('input', $event.target.value)"
+      />
+      <label
+        class="radio-wrapper-label"
+        :for="option"
+        :class="[option === chooseVal ? 'checked' : '']"
+      >
+        {{ option }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    default: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      chooseVal: this.default,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.radio {
+  &-container {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  &-wrapper {
+    &-input {
+      display: none;
+    }
+
+    &-label {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 120px;
+      height: 35px;
+      border-radius: 15px;
+      background: $color_white;
+      color: $color_green;
+      border: 1px $color_green solid;
+    }
+  }
+}
+
+.checked {
+  background: $color_green;
+  color: $color_white;
+  border: none;
+}
+</style>
